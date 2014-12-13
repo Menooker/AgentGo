9a9308c7be807218a725e44e725133ec73f4eaf6
Menooker <myjisgreat@live.cn>
2014-12-13 15:42:17 +0800

    add timing support for GTPAdapter

--- AgentGo/GTPAdapter.h
+++ AgentGo/GTPAdapter.h
@@ -8,13 +8,20 @@ protected:
 	Board bd;
 	float komi;
 private:
+#ifdef GO_TIME_STAT
+	double avgtime; 
+	long times;
+#endif
 	virtual void onClear()=0;
 	virtual void onBoardSize(int sz){}
 	virtual void onPlay(int isW,int a,int b)=0;
 	virtual bool onMove(int isW,int& a,int&b)=0;
 public:
 	void MainLoop();
-
+	GTPAdapter()
+	{
+		avgtime=0;times=0;
+	}
 };
 
 #endif
