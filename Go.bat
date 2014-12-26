9f39e33648cfe2df557c2c2448e9b16ccee2a0e2
Menooker <Menooker@users.noreply.github.com>
2014-12-26 23:09:36 +0800

    Create Go.bat

---
+++ Go.bat
@@ -0,0 +1,2 @@
+eset  GO=C:\Users\Menooker\Desktop\Go\gnugo-3.8\gnugo.exe --mode gtp --level 1
+D:\ProgramFiles\GoGui\gogui.exe -program "D:\ProgramFiles\GoGui\gogui-twogtp.exe -black D:\Menooker\vc\AI\AgentGo\Debug\AgentGo.exe -white \"%GO%\" -size 13"
